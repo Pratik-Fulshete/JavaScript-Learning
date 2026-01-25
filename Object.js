8e0fbd0123a26d7c5c329d959d0f10bfb8e76cc9
Pratik Fulshete <146758547+Pratik-Fulshete@users.noreply.github.com>
2026-01-25 23:02:46 +0530

    25 jan

--- Object.js
+++ Object.js
@@ -28,3 +28,48 @@ JsUser.greeting2 = function(){
     console.log(`Hello ${this.name}`) 
 }// using this keyword to access current Object properties
  console.log(JsUser.greeting2())
+//singleton using constructor
+
+//const tinderUser = new Object() // creating object using constructor
+const tinderUser={}
+//add properties to object
+tinderUser.id="1234"
+tinderUser.name = "Pranit"
+tinderUser.isLoggedIn = false
+//console.log(tinderUser)
+
+const regularUser = {
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"malti",
+            lastname:"chahar"
+        }
+    }
+}
+/*console.log(regularUser.fullname)
+console.log(regularUser.fullname.userfullname.firstname)// accessing nested object
+console.log(regularUser?.fullname?.userfullname)// using optional chaining*/
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+const obj3 = Object.assign({}, obj1, obj2)// merging multiple objects 
+//there are first parameter is target object and another are source objects
+//console.log(obj3)
+//merging using spread operator
+
+const obj4 = {...obj1,...obj2}
+//console.log( obj4)
+
+const users= [
+    {userId:1, userName:"pratik"},
+    {userId:2, userName:"abc"},
+    {userId:3, userName:"xyz"}
+
+]// array of  objects
+console.log(users[1].userName) //accessing object inside array
+
+//methods on objects
+console.log(Object.keys(tinderUser)) //it will gives array of keys
+console.log(Object.values(tinderUser))// it will gives array of values
+console.log(Object.entries(tinderUser))//it will gives array of [key, value] pairs means entries
+console.log(tinderUser.hasOwnProperty("name"))//it checks whether given key is present or not in object , it will gives answer in boolean (true/false).
