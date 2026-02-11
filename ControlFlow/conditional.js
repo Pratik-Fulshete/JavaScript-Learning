9b16e26c59f7974767c8448e0f0599400f37ccb3
Pratik Fulshete <146758547+Pratik-Fulshete@users.noreply.github.com>
2026-02-11 23:07:52 +0530

    control flow

---
+++ ControlFlow/conditional.js
@@ -0,0 +1,18 @@
+//if 
+ if (2==2){
+    console.log("2 is equal to 2")
+ }
+
+ //if else
+ const age = 18
+ if(age>=18){
+    console.log("eligible fo voting")
+ }else{
+    console.log("not eligible for voting")
+ }
+  
+  //short hand if else
+
+  const balance = 1000
+  balance>500 ? consolelog("sufficient balance"):console.log("in sufficient balance")
+  
