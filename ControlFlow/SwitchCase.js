3f5f2445b9432d6be98ffa333b571b86c116e49a
Pratik Fulshete <146758547+Pratik-Fulshete@users.noreply.github.com>
2026-02-12 22:49:07 +0530

    nullish coaliscing

---
+++ ControlFlow/SwitchCase.js
@@ -0,0 +1,14 @@
+const month = 3;
+switch (month){
+    case 1: // here we can write String , number or boolean value
+         console.log("January");
+        break;
+    case 2:
+         console.log("Feb");
+         break;
+    case 3:
+         console.log("March");
+         break;
+    default: 
+         console.log("Invaild month")
+}
--- ControlFlow/conditional.js
+++ ControlFlow/conditional.js
@@ -14,5 +14,56 @@
   //short hand if else
 
   const balance = 1000
-  balance>500 ? consolelog("sufficient balance"):console.log("in sufficient balance")
-  
+  balance>500 ? console.log("sufficient balance"):console.log("in sufficient balance")
+   
+   if(balance<500){
+      console.log("less than 500")
+   }else if(balance<750){
+      console.log("less than 750")
+   } 
+   const userLoggedIn = true;
+   const DebitCard=true;
+
+   if(userLoggedIn && DebitCard ){ // and operator
+      console.log("Allow to buy course")
+   }
+
+   if(userLoggedIn || DebitCard){ //or operator
+      console.log("Allow to buy course")
+   }
+   const userEmail = "pratik@gmail.com";
+   if(userEmail){
+      console.log("email is gotted!")
+   }else{
+      console.log("email is not gotted!")
+   }
+
+   //falsy values in js
+   // false, 0,-0,BigInt 0n,"" (empty String),null, undefined,NaN
+
+  //Truthy values in js
+  // true ," "(space in string), "0" (string with 0),
+  //[] (empty array),{} (empty object),function(){}(empty function)
+
+  //way of checking array in conditional Statement
+  const arr =[]
+  if(arr.length>0){
+   console.log("array is not empty")
+  }else{
+   console.log("array is empty")
+  }
+
+  // way of checking object in conditional Statement
+  const obj ={}
+  if(Object.keys(obj).length==0){
+   console.log("object is empty")
+  }
+  //Nullish coalescing operator(??):null or undefined value checking
+  let val1;
+  //val1 = 5 ?? 10;// here if first value is null or undefined then it will take 
+                 //2nd value otherwise it will take 1st value
+ //val1 = null ?? 10; //here answer is 10 because first value is null
+ //val1 = undefined ?? 10; // here also same answer 
+ val1 = null ?? 10 ?? 20;// here answer is 10 if second value also null or undefined it 
+                         // gives answer 20 ex: null?? undefined??20 
+  console.log(val1)
