fa001dfbcf33fb5b902f48142b453af5ae80d36b
Pratik Fulshete <146758547+Pratik-Fulshete@users.noreply.github.com>
2026-08-02 21:42:52 +0530

    async

---
+++ asyncjs/Proj1.js
@@ -0,0 +1,25 @@
+   const randomColor = function(){
+            const hex = '0123456789ABCDEF';
+            let color = '#';
+            for(let i =0 ; i<6;i++){
+              color += hex[Math.floor(Math.random()*16)];
+            }
+            return color;
+        }
+        let intervelId;
+        const startChangingColor = function(){ 
+            if(!intervelId){
+           intervelId = setInterval(Changer1 , 1000);
+            }
+            function Changer1(){
+            document.body.style.backgroundColor = randomColor();
+            }
+        };
+        const stopChangingColor = function(){
+            clearInterval(intervelId);
+            intervelId = null;
+        }
+        document.querySelector('#start').addEventListener('click', startChangingColor);
+        document.querySelector('#stop').addEventListener('click', stopChangingColor);
+     
+    
