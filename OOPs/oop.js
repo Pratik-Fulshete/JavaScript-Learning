72ae01cf76a057c6ec88ffff55bcebf641763487
Pratik Fulshete <146758547+Pratik-Fulshete@users.noreply.github.com>
2026-08-11 22:47:45 +0530

    prototype

--- OOPs/oop.js
+++ OOPs/oop.js
@@ -1,7 +1,30 @@
+//object literels
 const user ={
     username: "hitesh",
     loginCount : 8,
-    signedIn : true 
+    signedIn : true ,
+    getUserDetails: function(){
+        //console.log(`user name: ${this.username}`)
+        console.log(this) //it prints current whole object 
+    }
 }
 
-console.log(user.username);
+//console.log(user.username);
+//console.log(user.getUserDetails())
+
+//constructor function
+
+function User(userName,loginCount,isLoggedIn){
+    this.userName = userName
+    this.loginCount=loginCount
+    this.isLoggedIn=isLoggedIn
+    this.greeting = function (){
+        console.log(`Welcome ${this.userName}`)
+    }
+    return this; //not need to write (implicitly adds)
+
+    }
+    const userOne = new User("Pratik",12,true)
+    const userTwo = new User ("Rohit",10, false)
+    console.log(userOne.constructor);
+    //console.log(userTwo)
