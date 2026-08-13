cb78b466f2978304bfb002ca72b3887d681586c9
Pratik Fulshete <146758547+Pratik-Fulshete@users.noreply.github.com>
2026-08-13 22:52:18 +0530

    prototype

---
+++ OOPs/Prototype.js
@@ -0,0 +1,10 @@
+let MyName = "Pratik";
+console.log(MyName.length);
+let MyHeros=["Thor","SpiderMan"]
+let heropower={
+    thor:"hammer",
+    SpiderMan:"sling",
+    getSpiderPower:function(){
+        console.log(`SpiderMan power is ${this.SpiderMan}`);
+    }
+}
